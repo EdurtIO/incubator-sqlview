67e4b7eb95c5b1ed0a1b81cfb126c9d5c6f2b6b6
qianmoQ <shicheng@ttxit.com>
2020-12-30 19:50:05 +0800

    support redis connect and page

---
+++ connect/connect_redis.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python  
+#-*- coding:utf-8 _*-  
+""" 
+@Author: shicheng 
+@License: Apache Licence 
+@File: connect_redis.py 
+@Time: 2020-12-30 12:46
+@Contact: shicheng 
+@Site:  
+@Software: incubator-sqlview
+"""
---
+++ views/view_editor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python  
+#-*- coding:utf-8 _*-  
+""" 
+@Author: shicheng 
+@License: Apache Licence 
+@File: view_editor.py 
+@Time: 2020-12-30 12:27
+@Contact: shicheng 
+@Site:  
+@Software: incubator-sqlview
+"""
