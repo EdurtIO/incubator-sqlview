ae17ed2304df9a66247e1cc3a169f539d8de8736
qianmoQ <shicheng@ttxit.com>
2021-01-03 15:05:20 +0800

    Refactoring datasoure pages

--- views/__init__.py
+++ views/__init__.py
@@ -12,7 +12,9 @@
 from Configuration import Application, Logger
 from views.view_datasource import DataSourceView
 from views.view_user import UserView
+from views.view_writing import WritingView
 
 Logger.info('load views from view module')
 Application.register_blueprint(UserView, url_prefix='/user')
 Application.register_blueprint(DataSourceView, url_prefix='/datasource')
+Application.register_blueprint(WritingView, url_prefix='/writing')
